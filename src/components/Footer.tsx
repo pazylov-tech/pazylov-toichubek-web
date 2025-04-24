498cdc634c65e66adcf71356ffb9f5e5eb9287e0
pazylov-tech <pazylovtoichubek7@gmail.com>
2025-04-24 10:18:19 +0600

    Added some fixes to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,10 @@
-import { FaGithub, FaLinkedin, FaTelegram } from 'react-icons/fa';
+import {
+  FaGithub,
+  FaLinkedin,
+  FaTelegram,
+  FaInstagram,
+  FaEnvelope,
+} from 'react-icons/fa';
 
 export default function Footer() {
   return (
@@ -6,14 +12,14 @@ export default function Footer() {
       <div className="max-w-6xl mx-auto flex flex-col md:flex-row items-center justify-between gap-6 text-center md:text-left">
         {/* Left side */}
         <div>
-          <div className="flex items-center gap-2">
+          <div className="flex items-center gap-1">
             <img
               src="/logo.png"
               alt="logo"
               className="h-10 w-10 hover:text-blue-300 cursor-pointer"
             />
             <h3 className="text-xl font-semibold text-cyan-400">PAZYLOV</h3>
-            <p className="text-sm mt-1">
+            <p className="text-sm ml-4">
               © {new Date().getFullYear()} All rights reserved.
             </p>
           </div>
@@ -22,25 +28,39 @@ export default function Footer() {
         {/* Socials */}
         <div className="flex gap-6 text-2xl">
           <a
-            href="https://github.com/Toichubek"
+            href="https://github.com/pazylov-tech"
             target="_blank"
             rel="noopener noreferrer"
             className="hover:text-white transition duration-200">
             <FaGithub />
           </a>
           <a
-            href="https://linkedin.com/in/toichubek"
+            href="https://www.linkedin.com/in/toichubek-pazylov/"
             target="_blank"
             rel="noopener noreferrer"
             className="hover:text-white transition duration-200">
             <FaLinkedin />
           </a>
           <a
-            href="https://t.me/yourtelegramusername"
+            href="https://t.me/pazylovt"
             target="_blank"
             rel="noopener noreferrer"
             className="hover:text-white transition duration-200">
             <FaTelegram />
+          </a>
+          <a
+            href="https://www.instagram.com/pazylovv.t/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-white transition duration-200">
+            <FaInstagram />
+          </a>
+          <a
+            href="https://mail.google.com/mail/?view=cm&to=pazylovtoichubek7@gmail.com&subject=Hello&body=I%20saw%20your%20profile!"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-white transition duration-200">
+            <FaEnvelope />
           </a>
         </div>
       </div>
