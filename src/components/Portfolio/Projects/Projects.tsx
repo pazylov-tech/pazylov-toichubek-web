ef5e7a79156709f2b7468b439301336fca0f2c80
pazylov-tech <pazylovtoichubek7@gmail.com>
2025-06-17 22:04:22 +0600

    Did some changes with unexpected layout bags with the portfolio section, it needs further polishing.

--- src/components/Portfolio/Projects/Projects.tsx
+++ src/components/Portfolio/Projects/Projects.tsx
@@ -15,16 +15,16 @@ const Projects = () => {
       id="projects"
       data-aos="fade-up"
       data-aos-anchor-placement="top-center">
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 ml-90 gap-x-120 gap-y-5 place-items-center">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 ml-90 gap-x-110 gap-y-5 place-items-center">
         {projectsItems.map((project, index) => (
           <Link
             to={`/projects/${project.slug}`}
             key={index}
-            className="w-85 h-auto rounded-2xl shadow-md hover:scale-105 transition duration-300 overflow-hidden bg-[#112240] flex flex-col no-underline">
+            className="w-77 h-auto rounded-2xl shadow-md hover:scale-105 transition duration-300 overflow-hidden bg-[#112240] flex flex-col no-underline">
             <img
               src={project.image}
               alt={project.title}
-              className="rounded-t-2xl w-full h-50 object-cover"
+              className="rounded-t-2xl w-full h-45 object-cover"
             />
             <div className="p-4 text-center flex flex-col justify-between h-full pt-1">
               <h3 className="text-white text-lg font-semibold">
@@ -45,7 +45,7 @@ const Projects = () => {
                     Live Demo <ExternalLink size={14} />
                   </a>
                 )}
-                <span className="flex items-center gap-1 border border-pink-300 text-pink-300 hover:bg-pink-300 hover:text-[#112240] px-3 py-1 rounded-full transition duration-200">
+                <span className="flex items-center gap-1 border border-pink-300 text-pink-300 hover:bg-pink-300 hover:text-[#112240] px-2 rounded-full transition duration-200">
                   Details <ArrowRight size={14} />
                 </span>
               </div>
